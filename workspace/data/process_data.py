19f719f972e410e619a62a9647d6a39f3ea2cc8d
Jeffri Sandy <jeffri.sandy@gmail.com>
2018-10-14 12:05:42 +0700

    Finish ETL process_data

--- workspace/data/process_data.py
+++ workspace/data/process_data.py
@@ -1,16 +1,58 @@
 import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, on='id')
+    return df
 
 
 def clean_data(df):
-    pass
+    # create a dataframe of the 36 individual category columns
+    categories = df.categories.str.split(";",expand=True)
 
+    # select the first row of the categories dataframe
+    row = categories.loc[0,:]
+
+    # use this row to extract a list of new column names for categories.
+    category_colnames = categories.loc[0,:].str.split('-', expand=True)[0].values
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    #convert category values to just numbers 0 or 1
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str.split('-', expand=True)[1].values
+
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+
+    # drop the original categories column from `df`
+    df = df.drop(columns='categories')
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df,categories], axis=1)
+
+    #drop df.related == 2
+    if (df.related == 2).any():
+        df = df.drop(index = df[df.related == 2].index)
+
+    # check number of duplicates
+    duplicates = df[df.duplicated(['id'],keep='first')]
+
+    # drop duplicates
+    df = df.drop(index= duplicates.index)
+
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    df.to_sql('messages', engine, index=False, if_exists='replace')
 
 
 def main():
@@ -24,12 +66,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
